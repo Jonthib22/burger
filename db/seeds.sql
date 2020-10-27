3c3de63575866bfac3bac0a5096ca1a4af440ecd
jonthib22 <jonthib95@gmail.com>
2020-10-27 03:07:08 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+USE burgers_db
+
+-- Creates new rows containing data in all named columns --
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hot and Spicy", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BBQ Bacon Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("A1 Thick and Hearty", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", TRUE);
---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+const express = require("express");
+const mysql = require("mysql");
+
+// Create express app instance.
+const app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+// MySQL DB Connection Information (remember to change this with our specific credentials)
+const connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "dbpassword",
+    database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadID + "\n");
+});
